8f185d5f50275ea0172d367b7f7747a7425f93ff
leightonoff <leightoncot@Gmail.com>
2021-02-04 19:26:45 -0500

    Libray adds and UI change up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import helpers from "/.helpers.js";
+//import './Notepad';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+  <h2>Welcome to Notebook</h2>
+        <header className="App-header">
+        
+          <button className ="open">Open</button>
+          
+        </header>
+        
+      </div>
+      
+    );
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import firebase from "firebase";
+
+const firebase = require('firebase/app');
+require('firebase/firestore');
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgkQ2K9TrUgcToqmd_y-mt-7kXas51ABI",
+  authDomain: "notebook-12807.firebaseapp.com",
+  projectId: "notebook-12807",
+  storageBucket: "notebook-12807.appspot.com",
+  messagingSenderId: "967120550211",
+  appId: "1:967120550211:web:8bea95a5e273f8b6fed7f6",
+  measurementId: "G-86SKPJ296Q"
+};
+// Initialize Firebase
+firebase.default.initializeApp(firebaseConfig); 
+//firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
